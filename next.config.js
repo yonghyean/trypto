1524cc5d26422e28e814ef8970d826dba801544e
yonghyean <kyhyh0334@gmail.com>
2023-06-17 19:58:52 +0900

    chore: svgr 설정

--- next.config.js
+++ next.config.js
@@ -8,6 +8,27 @@ const nextConfig = {
       },
     ];
   },
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      // .svg로 끝나는 모든 파일을 변환합니다.
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
   images: {
     domains: ["ipfs-2.thirdwebcdn.com", "http://localhost:3000/"],
   },
