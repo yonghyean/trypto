5272f490505a722d312484b03cffccf18b355d94
yonghyean <kyhyh0334@gmail.com>
2023-06-18 16:03:56 +0900

    script: generate:icons

--- src/components/icons/Menu.tsx
+++ src/components/icons/Menu.tsx
@@ -3,9 +3,10 @@ import type { SVGProps } from "react";
 const SvgMenu = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={34}
-    height={34}
+    width="1em"
+    height="1em"
     fill="none"
+    viewBox="0 0 34 34"
     {...props}
   >
     <path
